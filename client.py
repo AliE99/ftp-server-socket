43807faf9602e9e1e2fadb043d304f3cfdcb3b9a
Ali Elahi <alielahi.1378@gmail.com>
2022-01-18 19:15:34 +0330

    Feat: connection between server and client is established

--- client.py
+++ client.py
@@ -1,7 +1,45 @@
 import socket
+# Initialise socket stuff
+ip = "127.0.0.1"
+port = 1234
+buffer_size = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+def connection():
+    # Connect to the server
+    print("Sending server request...")
+    try:
+        s.connect((ip, port))
+        print("Connection established successfully")
+    except:
+        print("Connection was unsucessful.")
+
+
+
+while True:
+    # Listen for a command
+    prompt = input("\nEnter a command: ")
+    if prompt[:4].upper() == "CONN":
+        connection()
+    elif prompt[:4].upper() == "UPLD":
+        print("under construction!")
+        # upld(prompt[5:])
+    elif prompt[:4].upper() == "LIST":
+        print("under construction!")
+        # list_files()
+    elif prompt[:4].upper() == "DWLD":
+        # dwld(prompt[5:])
+        print("under construction!")
+    elif prompt[:4].upper() == "DELF":
+        # delf(prompt[5:])
+        print("under construction!")
+    elif prompt[:4].upper() == "QUIT":
+        # quit()
+        print("under construction!")
+        break
+    else:
+        print("Command not recognised; please try again")
+
+
+
--- server.py
+++ server.py
@@ -1,11 +1,40 @@
 import socket
 
+
+ip = "127.0.0.1"
+port = 1234
+buffer_size = 1024
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-print(socket.gethostname())
+s.bind((ip, port))
+print(f"\nServer binded on ip: {ip} and port: {port}")
+
 s.listen(5)
+print(f"\nServer listening, waiting for connections...")
+
+clientsocket, address = s.accept()
+print(f"\nConnected to by address: {address}")
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    # Enter into a while loop to recieve commands from client
+    print("\n\nWaiting for instruction")
+    data = clientsocket.recv(buffer_size)
+    print("\nRecieved instruction: {}".format(data))
+    # Check the command and respond correctly
+    if data == "UPLD":
+        print('upload')
+        # upld()
+    elif data == "LIST":
+        print('list')
+        # list_files()
+    elif data == "DWLD":
+        print('download')
+        # dwld()
+    elif data == "DELF":
+        print('delete')
+        # delf()
+    elif data == "QUIT":
+        print('quit')
+        # quit()
+    # Reset the data to loop
+    data = None
