5b148bdff39c62039dbcdce1e97f3ff9108968d1
Ali Elahi <alielahi.1378@gmail.com>
2022-01-19 01:38:57 +0330

    Feat: server is now implemented with thread

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import struct
 
 # Initialise socket stuff
 ip = "127.0.0.1"
-port = 1234
+port = 1235
 buffer_size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import struct
 from _thread import *
 
 ip = "127.0.0.1"
-port = 1234
+port = 1235
 buffer_size = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,13 +14,13 @@ print(f"\nServer binded on ip: {ip} and port: {port}")
 s.listen(1)
 print(f"\nServer listening, waiting for connections...")
 
-clientsocket, address = s.accept()
-print(f"\nConnected to by address: {address}")
+# clientsocket, address = s.accept()
+# print(f"\nConnected to by address: {address}")
 
 
-def upload():
-    clientsocket.send("1".encode())
-    file_size = struct.unpack("i", clientsocket.recv(4))[0]
+def upload(connection, address):
+    connection.send("1".encode())
+    file_size = struct.unpack("i", connection.recv(4))[0]
     # Initialise and enter loop to recive file content
     start_time = time.time()
     output_file = open('received.jpg', "wb")
@@ -29,7 +29,7 @@ def upload():
     print("\nStart recieving the file:\n")
     chunk_number = 0
     while bytes_recieved < file_size:
-        l = clientsocket.recv(buffer_size)
+        l = connection.recv(buffer_size)
         print(f"Chunk {chunk_number} is Received...")
         chunk_number += 1
         output_file.write(l)
@@ -37,16 +37,14 @@ def upload():
     output_file.close()
     print("\nRecieved file: {}".format('received.jpg'))
     # Send upload performance details
-    clientsocket.send(struct.pack("f", time.time() - start_time))
-    clientsocket.send(struct.pack("i", file_size))
+    connection.send(struct.pack("f", time.time() - start_time))
+    connection.send(struct.pack("i", file_size))
     return
 
 
-
+threadCounter = 0
 while 1:
-    (clientConnetion, addr) = clientsocket.accept()
-    start_new_thread(upload, (clientConnetion, addr))
-    threadCounter = threadCounter+1
-    print()
+    (clientsocket, address) = s.accept()
+    start_new_thread(upload, (clientsocket, address))
+    threadCounter = threadCounter + 1
     print("Client "+str(threadCounter) + " is  Connected ")
-    print()
