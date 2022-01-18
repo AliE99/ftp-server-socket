22a623328f34933f1cf28659777805f0dfe0320c
Ali Elahi <alielahi.1378@gmail.com>
2022-01-19 01:33:06 +0330

    stage before thread

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import socket
+import sys
+import os
+import struct
+
 # Initialise socket stuff
 ip = "127.0.0.1"
 port = 1234
 buffer_size = 1024
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def connection():
     # Connect to the server
@@ -16,30 +20,45 @@ def connection():
         print("Connection was unsucessful.")
 
 
+def upload(file_name):
+    # Upload a file
+    print("\nUploading file: {}...".format(file_name))
+    try:
+        # Check the file exists
+        content = open(file_name, "rb")
+    except:
+        print("Couldn't open file. Make sure the file name was entered correctly.")
+        return
+    try:
+        s.recv(buffer_size)
+        s.send(struct.pack("i", os.path.getsize(file_name)))
+    except:
+        print("Error sending file details")
+    try:
+        # Send the file in chunks defined by BUFFER_SIZE
+        # Doing it this way allows for unlimited potential file sizes to be sent
+        chunk_number = 0
+        l = content.read(buffer_size)
+        print("\n Start sending the file:\n")
+        while l:
+            s.send(l)
+            print(f"Chunk {chunk_number} is sent...")
+            chunk_number += 1
+            l = content.read(buffer_size)
+        content.close()
+        # Get upload performance details
+        upload_time = struct.unpack("f", s.recv(4))[0]
+        upload_size = struct.unpack("i", s.recv(4))[0]
+        print("\nSent file: {}\nTime elapsed: {}s\nFile size: {}b".format(
+            file_name, upload_time, upload_size))
+    except:
+        print("Error sending file")
+        return
+    return
+
 
 while True:
+    connection()
+    upload('icon.jpg')
     # Listen for a command
-    prompt = input("\nEnter a command: ")
-    if prompt[:4].upper() == "CONN":
-        connection()
-    elif prompt[:4].upper() == "UPLD":
-        print("under construction!")
-        # upld(prompt[5:])
-    elif prompt[:4].upper() == "LIST":
-        print("under construction!")
-        # list_files()
-    elif prompt[:4].upper() == "DWLD":
-        # dwld(prompt[5:])
-        print("under construction!")
-    elif prompt[:4].upper() == "DELF":
-        # delf(prompt[5:])
-        print("under construction!")
-    elif prompt[:4].upper() == "QUIT":
-        # quit()
-        print("under construction!")
-        break
-    else:
-        print("Command not recognised; please try again")
-
-
-
+    prompt = input("\n")
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
-
+import time
+import struct
+from _thread import *
 
 ip = "127.0.0.1"
 port = 1234
@@ -9,32 +11,42 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((ip, port))
 print(f"\nServer binded on ip: {ip} and port: {port}")
 
-s.listen(5)
+s.listen(1)
 print(f"\nServer listening, waiting for connections...")
 
 clientsocket, address = s.accept()
 print(f"\nConnected to by address: {address}")
 
-while True:
-    # Enter into a while loop to recieve commands from client
-    print("\n\nWaiting for instruction")
-    data = clientsocket.recv(buffer_size)
-    print("\nRecieved instruction: {}".format(data))
-    # Check the command and respond correctly
-    if data == "UPLD":
-        print('upload')
-        # upld()
-    elif data == "LIST":
-        print('list')
-        # list_files()
-    elif data == "DWLD":
-        print('download')
-        # dwld()
-    elif data == "DELF":
-        print('delete')
-        # delf()
-    elif data == "QUIT":
-        print('quit')
-        # quit()
-    # Reset the data to loop
-    data = None
+
+def upload():
+    clientsocket.send("1".encode())
+    file_size = struct.unpack("i", clientsocket.recv(4))[0]
+    # Initialise and enter loop to recive file content
+    start_time = time.time()
+    output_file = open('received.jpg', "wb")
+    # This keeps track of how many bytes we have recieved, so we know when to stop the loop
+    bytes_recieved = 0
+    print("\nStart recieving the file:\n")
+    chunk_number = 0
+    while bytes_recieved < file_size:
+        l = clientsocket.recv(buffer_size)
+        print(f"Chunk {chunk_number} is Received...")
+        chunk_number += 1
+        output_file.write(l)
+        bytes_recieved += buffer_size
+    output_file.close()
+    print("\nRecieved file: {}".format('received.jpg'))
+    # Send upload performance details
+    clientsocket.send(struct.pack("f", time.time() - start_time))
+    clientsocket.send(struct.pack("i", file_size))
+    return
+
+
+
+while 1:
+    (clientConnetion, addr) = clientsocket.accept()
+    start_new_thread(upload, (clientConnetion, addr))
+    threadCounter = threadCounter+1
+    print()
+    print("Client "+str(threadCounter) + " is  Connected ")
+    print()
